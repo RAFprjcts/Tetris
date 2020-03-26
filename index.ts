9dbe569e846f57d0429bb0b1a9d5cb3ed743331d
Rafik Abdryakhimov <raaafik95@gmail.com>
2020-03-27 01:09:49 +0300

    Edit index.ts use view

--- index.ts
+++ index.ts
@@ -1,11 +1,35 @@
 import Game from "./src/game";
+import View from "./src/view";
 
 declare global {
-    interface Window { game: any; }
+    interface Window { game: any; view: any; }
 }
 
+const root = document.querySelector('#root');
+
 const game = new Game();
+const view = new View(root, 480, 640, 20, 10);
 
 window.game = game;
+window.view = view;
 
-console.log(game);
+document.addEventListener('keydown', event => {
+    switch (event.keyCode) {
+        case 37:
+            game.movePieceLeft();
+            view.render(game.getState());
+            break;
+        case 38:
+            game.rotatePiece();
+            view.render(game.getState());
+            break;
+        case 39:
+            game.movePieceRight();
+            view.render(game.getState());
+            break;
+        case 40:
+            game.movePieceDown();
+            view.render(game.getState());
+            break;
+    }
+});
