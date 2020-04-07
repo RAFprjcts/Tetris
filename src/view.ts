701aa4f7741aba1c6246f360919d118f8ef9788c
Rafik Abdryakhimov <raaafik95@gmail.com>
2020-04-07 20:44:45 +0300

    Add colors list

--- src/view.ts
+++ src/view.ts
@@ -1,4 +1,13 @@
 export default class View {
+    static colors = {
+        '1': 'cyan',
+        '2': 'blue',
+        '3': 'orange',
+        '4': 'yellow',
+        '5': 'green',
+        '6': 'purple',
+        '7': 'red'
+    };
     element: any;
     width: number;
     height: number;
@@ -42,7 +51,7 @@ export default class View {
                 const block = line[x];
 
                 if (block) {
-                this.renderBlock(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight, 'red');
+                    this.renderBlock(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight, View.colors[block]);
                 }
             }
         }
